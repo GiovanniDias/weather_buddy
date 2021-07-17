22c949c9d1e364a4c46f30de7b4872e773181145
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2021-07-17 10:38:08 -0300

    feat: Handle input data
    - Set input focus on page load
    - Bind input data with component store
    - Call handlers to each event
    - Adding request calling

--- src/components/Content/InputMessage.js
+++ src/components/Content/InputMessage.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { fetchData } from '../../services/requests'
 
-const InputMessage = () => (
-  <div className="content-text">
-    How is the weather in
-    <input type="text"/>
-    now?
-  </div>
-)
+const InputMessage = () => {
+  const [data, setData] = useState("")
 
+  const handleChange = (event) => {
+    setData(event.target.value)
+  }
+
+  const handleBlur = () => {
+    if(data) fetchData(data)
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === ''){
+      fetchData(data)
+    }
+  }
+  
+  return (
+    <div className="content-text">
+      How is the weather in
+      <input
+        autoFocus
+        type="text" 
+        value={data}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onKeyPress={handleKeyPress}
+      />
+      now?
+    </div>
+  )
+}
 export default InputMessage;
---
+++ src/services/requests.js
@@ -0,0 +1,3 @@
+export const fetchData = () => {
+  alert('Testing')
+}
